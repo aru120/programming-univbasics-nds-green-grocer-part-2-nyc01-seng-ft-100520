8446c5160425549205c38c1cb541fc9eff3f5b39
Kevin Huang <khuang159@gmail.com>
2020-09-29 14:34:17 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -8,7 +8,7 @@ def apply_coupons(cart, coupons)
  
  coupons.each do |key|
  item = find_item_by_name_in_collection(key[:item],cart)
- coupon_name = "#{coupons[i][:item]} W/COUPON"
+ coupon_name = "#{key[:item]} W/COUPON"
  
  binding.pry
  i+=1
